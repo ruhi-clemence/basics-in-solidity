ea0ec3bb2e0478f40e181f5b6815da45aaca13a0
Ruhi <54887515+ruhi-clemence@users.noreply.github.com>
2024-12-10 11:18:21 +0300

    Update ZKSyncDevOps.t.sol

--- test/unit/ZKSyncDevOps.t.sol
+++ test/unit/ZKSyncDevOps.t.sol
@@ -7,7 +7,7 @@ import { ZkSyncChainChecker } from "lib/foundry-devops/src/ZkSyncChainChecker.so
 import { FoundryZkSyncChecker } from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
 
 contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
-    // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
+    // Remove the `skipZkSync`, then run `forge test --match-test testZkSyncChainFails --zksync` and this will fail!
     function testZkSyncChainFails() public skipZkSync {
         address ripemd = address(uint160(3));
 
