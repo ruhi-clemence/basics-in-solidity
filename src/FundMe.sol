82d478894e099f450db5b4f6464b4df53690ced3
Ruhi <54887515+ruhi-clemence@users.noreply.github.com>
2024-12-10 11:10:16 +0300

    Update FundMe.sol

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -65,7 +65,7 @@ contract FundMe {
 
         // call
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
+
     // Ether is sent to contract
     //      is msg.data empty?
     //          /   \
@@ -126,11 +126,3 @@ contract FundMe {
     }
 }
 
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
